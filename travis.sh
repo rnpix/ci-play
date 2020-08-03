141f61a418894272c6aa6e6b940e55c259cc4f44
robnagler <github@q33.us>
2020-08-03 17:17:18 +0000

    test #2

---
+++ travis.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -eou pipefail
+if ! [[ $TRAVIS_BRANCH == master && $TRAVIS_EVENT_TYPE == push ]]; then
+    echo "not running $TRAVIS_BRANCH $TRAVIS_EVENT_TYPE"
+    exit
+fi
+docker run -u vagrant -i radiasoft/python3 /bin/bash -l <<'EOF'
+set -eou pipefail
+set -x
+mkdir -p $HOME/src/radiasoft
+cd $HOME/src/radiasoft
+gcl pykern
+cd pykern
+pip install -q -e .
+pykern github update_alpha_pending robnagler/ci-play
+EOF
