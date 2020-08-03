298f9a9b2146b15bda9d6799b676e223bab3a7db
robnagler <github@q33.us>
2020-08-03 17:19:42 +0000

    try #2 again

--- travis.sh
+++ travis.sh
@@ -4,7 +4,7 @@ if ! [[ $TRAVIS_BRANCH == master && $TRAVIS_EVENT_TYPE == push ]]; then
     echo "not running $TRAVIS_BRANCH $TRAVIS_EVENT_TYPE"
     exit
 fi
-docker run -u vagrant -i radiasoft/python3 /bin/bash -l <<'EOF'
+docker run -u vagrant -e PYKERN_PKCLI_GITHUB_USER -e PYKERN_PKCLI_GITHUB_USER -i radiasoft/python3:alpha /bin/bash -l <<'EOF'
 set -eou pipefail
 set -x
 mkdir -p $HOME/src/radiasoft
